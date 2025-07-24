07e6b54e4160dd15280dd7f08cb3903e31572793
nvdemidenko <n.v.demidenko@gmail.com>
2025-07-24 17:51:27 +0300

    add SolutionsEnterprize project

--- src/test/java/SolutionsEnterprize.java
+++ src/test/java/SolutionsEnterprize.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.Configuration;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -12,12 +13,12 @@ public class SolutionsEnterprize {
     @BeforeAll
     static void browserSetUp() {
         Configuration.pageLoadStrategy = "eager";
-        Configuration.baseUrl = "https://github.com";
+        baseUrl = "https://github.com";
     }
 
     @Test
     void solutionsEnterprizeTest() {
-        open();
+        open(baseUrl);
         $(byTagAndText("button", "Solutions")).hover();
         $(byTagAndText("a", "Enterprise")).click();
         $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" + "developer platform"));
